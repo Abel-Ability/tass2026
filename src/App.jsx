976c0e89b977475cc6766a32c8a68e80b02b76c5
Ability <abel.osagie@uniabuja.edu.ng>
2026-07-02 08:28:28 +0100

    Add basename=/tass2026 to BrowserRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <ThemeProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
+        <Router basename="/tass2026">
           <ScrollToTop />
           <Routes>
             <Route element={<Layout />}>
